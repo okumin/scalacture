185fb038b56eb7d4d8dc36a845f64b0d0f4edc4f
okumin <mail@okumin.com>
2016-06-18 17:53:18 +0900

    Add the constant ln 2

--- scalacture-experimental/src/main/scala/scalacture/experimental/util/Math.scala
+++ scalacture-experimental/src/main/scala/scalacture/experimental/util/Math.scala
@@ -1,5 +1,7 @@
 package scalacture.experimental.util
 
 private[scalacture] object Math {
-  def log2(x: Double): Double = math.log(x) / math.log(2.0)
+  val Ln2 = math.log(2.0)
+
+  def log2(x: Double): Double = math.log(x) / Ln2
 }
